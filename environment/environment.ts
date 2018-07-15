eed0805e51967b490fddf7cfe30d99d9dd5e1331
francislagueu <lagson6@yahoo.fr>
2018-07-15 16:42:14 -0700

    remove firebase keys

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDVIZNFTS5S0aN9_PLW6vQylhHgMmT_whk",
-    authDomain: "kidsplayapp-91233.firebaseapp.com",
-    databaseURL: "https://kidsplayapp-91233.firebaseio.com",
-    projectId: "kidsplayapp-91233",
-    storageBucket: "kidsplayapp-91233.appspot.com",
-    messagingSenderId: "752520521312"
+ 
 };
